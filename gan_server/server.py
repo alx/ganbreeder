2460fdedbeae468b24f83cbb3230171ab81839eb
Denis Shatilov <denis.shatilov.ctr@nps.edu>
2019-06-11 17:35:46 -0700

    Running in docker

--- gan_server/server.py
+++ gan_server/server.py
@@ -186,4 +186,5 @@ def mix_images():
 if __name__ == '__main__':
     port = int(sys.argv[1]) if len(sys.argv) > 1 else 5000
     print('port=', port)
+    app.run(use_reloader=False)
     app.run(host='0.0.0.0', debug=True, port=port)
